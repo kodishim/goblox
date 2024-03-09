38a134ae7c4ed34ce631fc5b9f3a5e63e9868e22
Kodi <kodi@Wunlims-MacBook-Pro.local>
2024-03-09 18:00:58 -0500

    FetchCatalogItemsData can now request over 100 items per call.

--- roscraper/catalog.go
+++ roscraper/catalog.go
@@ -2,7 +2,6 @@ package roscraper
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"net/http"
 
@@ -21,37 +20,44 @@ func (r *Roscraper) FetchCatalogItemData(itemID int) (*robloxapi.CatalogItemData
 }
 
 func (r *Roscraper) FetchCatalogItemsData(itemIDs ...int) ([]*robloxapi.CatalogItemData, error) {
-	if len(itemIDs) > 100 {
-		return nil, errors.New("error can't fetch more than 100 item's catalog data at once")
+	catalogItemsData := []*robloxapi.CatalogItemData{}
+	batches := [][]int{}
+	for i := 0; i < len(itemIDs); i += 100 {
+		end := i + 100
+		if end > len(itemIDs) {
+			end = len(itemIDs)
+		}
+		batches = append(batches, itemIDs[i:end])
 	}
-	url := robloxapi.CatalogAPI + "/catalog/items/details"
-	type item struct {
-		ItemType int `json:"itemType"`
-		ID       int `json:"id"`
+	for _, batch := range batches {
+		url := robloxapi.CatalogAPI + "/catalog/items/details"
+		type item struct {
+			ItemType int `json:"itemType"`
+			ID       int `json:"id"`
+		}
+		var body struct {
+			Items []item `json:"items"`
+		}
+		for _, itemID := range batch {
+			body.Items = append(body.Items, item{1, itemID})
+		}
+		jsonBody, err := json.Marshal(body)
+		if err != nil {
+			return nil, fmt.Errorf("error marshaling response body: %w", err)
+		}
+		resp, err := r.Request(http.MethodPost, url, jsonBody)
+		if err != nil {
+			return nil, fmt.Errorf("error making request: %w", err)
+		}
+		if resp.APIError != nil {
+			return nil, resp.APIError
+		}
+		var respBody robloxapi.FetchCatalogItemsDataResponse
+		err = json.Unmarshal(resp.Body, &respBody)
+		if err != nil {
+			return nil, fmt.Errorf("error unmarshaling response body: %w", err)
+		}
+		catalogItemsData = append(catalogItemsData, respBody.ItemsData...)
 	}
-	var body struct {
-		Items []item `json:"items"`
-	}
-	body.Items = []item{}
-	for _, itemID := range itemIDs {
-		body.Items = append(body.Items, item{1, itemID})
-	}
-	jsonBody, err := json.Marshal(body)
-	if err != nil {
-		return nil, fmt.Errorf("error marshaling response body: %w", err)
-	}
-	resp, err := r.Request(http.MethodPost, url, jsonBody)
-	if err != nil {
-		return nil, fmt.Errorf("error making request: %w", err)
-	}
-	if resp.APIError != nil {
-		return nil, resp.APIError
-	}
-	var respBody robloxapi.FetchCatalogItemsDataResponse
-	err = json.Unmarshal(resp.Body, &respBody)
-	if err != nil {
-		return nil, fmt.Errorf("error unmarshaling response body: %w", err)
-	}
-	return respBody.ItemsData, nil
-
+	return catalogItemsData, nil
 }
